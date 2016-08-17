4a97e524c18906d1a6b6d8620370a11cb9c739d2
shwinpiocess <springwind_lee@163.com>
2016-08-17 23:02:07 +0800

    add get ssh version function

--- job/utils.py
+++ job/utils.py
@@ -11,3 +11,14 @@ def get_ansible_version():
         return stripped_result.split('\n', 1)[0]
     except:
         return 'unknown'
+
+def get_ssh_version():
+    """
+    Return SSH version installed.
+    """
+    try:
+        proc = subprocess.Popen(['ssh', '-V'], stderr=subprocess.PIPE)
+        result = proc.communicate()[1]
+        return result.split(' ')[0].split('_')[1]
+    except:
+        return 'unknown'
