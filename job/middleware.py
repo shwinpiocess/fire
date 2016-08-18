d7db1e7d5387a9ae024f9cbc6560d2f35e0bd54a
shwinpiocess <springwind_lee@163.com>
2016-08-18 16:23:44 +0800

    exclude inventories url

--- job/middleware.py
+++ job/middleware.py
@@ -6,5 +6,8 @@ class LoginRequiredMiddleware(object):
 
     def process_request(self, request):
         login_url = reverse('job:login')
+        inventories_url = reverse('job:inventories')
+        if request.path == inventories_url:
+            return
         if not request.user.is_authenticated() and request.path != login_url:
             return redirect(login_url)
